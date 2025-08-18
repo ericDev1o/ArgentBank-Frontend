0c9b0f9635175b55a01be69a09791c7f9b56211b
Eric Faraut <eric.faraut@hotmail.fr>
2025-08-18 10:58:23 +0200

    Error page layout header is done.

--- ArgentBank-FrontEnd/src/pages/Error404.tsx
+++ ArgentBank-FrontEnd/src/pages/Error404.tsx
@@ -1,7 +1,7 @@
 import Layout from "../components/containers/Layout";
 
 export default function Error404() {
-    return <Layout>
+    return <Layout logIn={true}>
         <main>
             <h1>Erreur 404</h1>
             <h2>Page non trouvée</h2>
