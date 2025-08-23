3b5653fb4ca1231c99674d8900eb8af8b570b7f6
Eric Faraut <eric.faraut@hotmail.fr>
2025-08-23 18:33:57 +0200

    Auth loss at user page reload is fixed.

---
+++ src/app/persistConfig.ts
@@ -0,0 +1,12 @@
+import storage from 'redux-persist/lib/storage'
+import { PersistConfig } from 'redux-persist'
+
+import { RootState } from './store'
+
+const persistConfig: PersistConfig<RootState> = {
+    key: 'root',
+    storage,
+    whitelist: ['connect']
+}
+
+export default persistConfig
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { persistReducer, persistStore } from 'redux-persist'
+import { PersistPartial } from 'redux-persist/lib/persistReducer'
 
 import { connectSlice } from '../features/connect/connectSlice'
 import { getProfileSlice } from '../features/getProfile/getProfileSlice'
+import persistConfig from './persistConfig'
 
-export const argentBankStore = configureStore({
-    reducer: {
-        connect: connectSlice.reducer,
-        profile: getProfileSlice.reducer
-    }
+const rootReducer = combineReducers({
+    connect: connectSlice.reducer,
+    profile: getProfileSlice.reducer
 })
+
+export type RootState = ReturnType<typeof rootReducer>
+type PersistedState = RootState & PersistPartial
+type ConnectActions = ReturnType<typeof connectSlice.actions[keyof typeof connectSlice.actions]>
+type AppActions = ConnectActions // | ProfileActions
+
+const persistedReducer = persistReducer(persistConfig, rootReducer) as (
+  state: PersistedState | undefined,
+  action: AppActions
+) => PersistedState;
+
+export const argentBankStore = configureStore({ 
+    reducer: persistedReducer
+})
+
+export const persistor = persistStore(argentBankStore)
