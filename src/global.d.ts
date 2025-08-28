254734931a34f0099b0b95db794d02553bf9fec5
Eric Faraut <eric.faraut@hotmail.fr>
2025-08-28 12:16:27 +0200

    Global environment is unpolluted by tests.

--- src/global.d.ts
+++
@@ -1,8 +0,0 @@
-import { TextEncoder as NodeTextEncoder, TextDecoder as NodeTextDecoder } from 'util';
-
-declare global {
-  var TextEncoder: typeof NodeTextEncoder;
-  var TextDecoder: typeof NodeTextDecoder;
-}
-
-export {};
